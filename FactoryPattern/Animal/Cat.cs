7e20a9ec23887814c241971ecf7e3dec269da36a
Sigh <yxh644166727@gmail.com>
2023-07-06 13:35:28 -0400

    Add files via upload

---
+++ FactoryPattern/Animal/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.Animal
+{
+    internal class Cat : IAnimal
+    {
+        public void Display()
+        {
+            Console.WriteLine("This is a Cat");
+        }
+    }
+}
---
+++ FactoryPattern/Factory/BalancedAnimalFactory.cs
@@ -0,0 +1,34 @@
+﻿using FactoryPattern.Animal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.Factory
+{
+    internal class BalancedAnimalFactory : IAnimalFactory
+    {
+        Random _rng;
+        List<IAnimal> _animals;
+        public BalancedAnimalFactory()
+        {
+            _rng = new Random();
+            _animals = new List<IAnimal> { new Cat(), new Dog(), new Duck()};
+            Console.WriteLine("Entering Balanced Animal Factory");
+        }
+        public IAnimal CreateAnimal()
+        {
+            if(_animals.Count == 0)
+                _animals = new List<IAnimal> { new Cat(), new Dog(), new Duck() };
+
+            var idx = _rng.Next(_animals.Count);
+
+            var animal = _animals[idx];
+
+            _animals.RemoveAt(idx);
+
+            return animal;
+        }
+    }
+}
