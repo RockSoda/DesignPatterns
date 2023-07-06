3a7225c3745a12e12511c0fe59b159c18a195e9b
Sigh <yxh644166727@gmail.com>
2023-07-06 17:17:15 -0400

    Add files via upload

---
+++ AbstractFactoryPattern/AbstractFacotry/IFactory.cs
@@ -0,0 +1,15 @@
+﻿using AbstractFactoryPattern.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern.AbstractFacotry
+{
+    internal interface IFactory
+    {
+        IProductA GetProductA();
+        IProductB GetProductB();
+    }
+}
