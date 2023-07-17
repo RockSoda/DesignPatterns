848c972a9160806f7dcebcb587a417a2aa815e1f
Sigh <yxh644166727@gmail.com>
2023-07-17 14:09:59 -0400

    Add files via upload

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using SingletonPattern.Singleton;
+
+namespace SingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonInstance.GetSingletonInstance();
+            SingletonInstance.GetSingletonInstance();
+        }
+    }
+}
