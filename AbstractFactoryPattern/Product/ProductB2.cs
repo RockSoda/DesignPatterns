57fd4b6dc08547ba2285b70b0464f38086011004
Sigh <yxh644166727@gmail.com>
2023-07-26 12:52:16 -0400

    Add files via upload

---
+++ AbstractFactoryPattern/Product/ProductB2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern.Product
+{
+    internal class ProductB2 : IProductB
+    {
+        public void Display()
+        {
+            Console.WriteLine("This is a Product of B2");
+        }
+    }
+}
