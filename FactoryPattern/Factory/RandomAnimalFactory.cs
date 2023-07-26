0f12a665374e687b24910a83a5026cbd8d437976
Sigh <yxh644166727@gmail.com>
2023-07-26 12:54:05 -0400

    Add files via upload

---
+++ FactoryPattern/Factory/RandomAnimalFactory.cs
@@ -0,0 +1,31 @@
+﻿using FactoryPattern.Animal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.Factory
+{
+    internal class RandomAnimalFactory : IAnimalFactory
+    {
+        Random _rng;
+        public RandomAnimalFactory() 
+        {
+            _rng = new Random();
+            Console.WriteLine("Entering Random Animal Factory");
+        }
+        public IAnimal CreateAnimal()
+        {
+            var rnd = _rng.Next(3);
+
+            return rnd switch
+            {
+                0 => new Cat(),
+                1 => new Dog(),
+                2 => new Duck(),
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using FactoryPattern.Factory;
+
+namespace FactoryPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimalFactory animalFactory;
+
+            var rng = new Random();
+
+            animalFactory = rng.Next(2) == 0 ? new BalancedAnimalFactory() : new RandomAnimalFactory();
+
+            for (int i = 0; i < 6; i++)
+                animalFactory.CreateAnimal().Display();
+
+        }
+    }
+}
